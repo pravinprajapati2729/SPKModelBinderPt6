6b963468082427630a58d743de318448ff526f5f
Pravin Prajapati <pravinprajapati2729@gmail.com>
2020-11-22 14:39:59 +0530

    Commit Changes

--- SPKModelBinderPt6/Models/Customer.cs
+++ SPKModelBinderPt6/Models/Customer.cs
@@ -6,7 +6,7 @@ using System.Web;
 namespace SPKModelBinderPt6.Models
 {
     public class Customer
-    {
+    {//Tes brancheck save
         public string CustomerCode { get; set; }
         public string CustomerName { get; set; }
     }
